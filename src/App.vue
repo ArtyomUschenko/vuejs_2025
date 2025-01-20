547c98eb7a3807a9e64551e4428ceef7b99a7ff5
Artyom <net775@yandex.ru>
2025-01-20 18:02:28 +0300

    Добавлен счетчик в проект

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="">Количесво: <strong>{{counter}}</strong></div>
+  <div class="">Счетчик: <strong>{{discounter}}</strong></div>
+  <button v-on:click="addcounter()">Увеличить</button>
+  <button @click="discounter = discounter -1">Уменьшить</button>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        counter: 0,
+        discounter: 0
+      }
+    },
+    methods: {
+      addcounter() {
+        this.counter = this.counter + 1;
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
